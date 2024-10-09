1fb4217a6b93073268ac5574b9c1f6ce94ab7ad8
Graham <gnbak09@gmail.com>
2024-10-09 17:45:35 -0500

    Update RobotContainer.java

--- RobotContainer.java
+++ RobotContainer.java
@@ -2,11 +2,13 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 //FIND THE IMPORT for the CommandXboxController in the API Documentation
-
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController
 public class RobotContainer {
   private final CommandXboxController joystick = new CommandXboxController(0);
-  //CREATE ANOTHER CommandXboxController with a different name and different port Number
+  private final CommandXboxController joystick2 = new CommandXboxController(1)
+//CREATE ANOTHER CommandXboxController with a different name and different port Number
   public static TalonFX shooterTop = new TalonFX(0);
+  public static TalonFX shooterBottom = new TalonFX(1);
   //CREATE ANOTHER TalonFX motor and call it "shooterBottom" and different port Number
 
 
@@ -25,13 +27,18 @@ public class RobotContainer {
 			.onFalse(
 				ButtonCommands.resetShooter(shooterTop, shooterBottom));
 
-     // CREATE A NEW FUNCTION FOR THE OTHER JOYSTICK YOU CREATED
+    joystick2
+	  // CREATE A NEW FUNCTION FOR THE OTHER JOYSTICK YOU CREATED
 	  //Instead of ".rightTrigger()" do ".leftTrigger()"
+	  .rightTrigger()
 	  .on True(
+	    	ButtonCommands.resetShooter(shooterTop, shooterBottom))
 		  //call on function to reset the Shooter)
 	  .whileTrue(
+	    		ButtonCommands.shootNote(shooterTop, shooterBottom, ShuffleBoard.TopShooterOutSpeed, ShuffleBoard.BottomShooterOutSpeed)
 		  //call on function to intakeNote)
 	  .onFalse(
+		  ButtonCommands.resetShooter(shooterTop, shooterBottom));
 		  //call on function to reset the Shooter)
 		  
     
